21cd721069a580402a8cf5ada370c4e00a72d62d
vlaunir@prophecy.io <vlaunir@prophecy.io>
2024-08-29 19:33:46 +0000

    Updates schema for raw_dynamic_table dataset and comments out imports in read_dynamic_source

--- prophecy/pipelines/raw_bronze_subgraph/code/raw_bronze_subgraph/graph/load_csv_data/read_dynamic_source.py
+++ prophecy/pipelines/raw_bronze_subgraph/code/raw_bronze_subgraph/graph/load_csv_data/read_dynamic_source.py
@@ -7,11 +7,11 @@ from .config import *
 from raw_bronze_subgraph.functions import *
 
 def read_dynamic_source(spark: SparkSession) -> DataFrame:
-    from pyspark.sql import *
-    from pyspark.sql.functions import *
-    from pyspark.sql.types import *
-    from prophecy.utils import *
-    from prophecy.libs import typed_lit
+    #from pyspark.sql import *
+    #from pyspark.sql.functions import *
+    #from pyspark.sql.types import *
+    #from prophecy.utils import *
+    #from prophecy.libs import typed_lit
     #from raw_bronze_subgraph.config.ConfigStore import *
     #from raw_bronze_subgraph.functions import *
     file_name = Config.source_path
